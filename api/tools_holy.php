8bc14060d2293c8f434ca1dca0df3d9b75380fe1
Newbilius <newbilius@gmail.com>
2012-10-31 22:10:55 +0600

    Добавлена функция GetMIME - возвращает MIME-тип файла. Требует установки расширения finfo, или mime_content_type, или утилиту file на сервере.

--- api/tools_holy.php
+++ api/tools_holy.php
@@ -295,6 +295,28 @@ function GetWordForms($count,$forms)
 };
 
 /**
+ * Возвращет MIME тип файла.
+ * 
+ * @param string $file_name <p>путь к файлу.</p>
+ */
+function GetMIME($file_name) {
+    if (function_exists("finfo_file")) {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+        $mime = finfo_file($finfo, $file_name);
+        finfo_close($finfo);
+        return $mime;
+    } else if (function_exists("mime_content_type")) {
+        return mime_content_type($file_name);
+    } else if (!stristr(ini_get("disable_functions"), "shell_exec")) {
+        $file = escapeshellarg($file_name);
+        $mime = shell_exec("file -bi " . $file);
+        return $mime;
+    } else {
+        return "";
+    }
+}
+
+/**
  * Возвращает полный адрес страницы по её id или name
  *
  * @param string/id $id  <p>id/name</p>
