3b08cd22432dd3b1b1e3cf7132d70abebcc4d263
Newbilius <newbilius@gmail.com>
2012-11-14 21:42:06 +0600

    fix

--- _install/site/templates/global_header.php
+++ _install/site/templates/global_header.php
@@ -8,7 +8,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
 <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />	
 <link rel="stylesheet" type="text/css" href="/css/style.css" />
-<script type="text/javascript" src="/engine/js/jquery-1.7.min.js"></script>
+<script type="text/javascript" src="/engine/js/jquery.1.8.3.min.js"></script>
 <script type="text/javascript" src="/engine/js/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
 <script type="text/javascript" src="/engine/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 <link rel="stylesheet" type="text/css" href="/engine/js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
