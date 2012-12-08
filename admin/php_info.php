50a4f09d9726437152d916a601e9a90c60d7943f
Newbilius <newbilius@gmail.com>
2012-12-08 21:05:20 +0600

    Поправлены стили phpinfo()

--- admin/php_info.php
+++ admin/php_info.php
@@ -1,4 +1,25 @@
 <?
 $_global_bread[] = Array("Информация о PHP");
+ob_start();
 phpinfo();
+$pinfo = ob_get_contents();
+ob_end_clean();
+$pinfo = preg_replace( '%^.*<body>(.*)</body>.*$%ms','$1',$pinfo);
+echo $pinfo;
 ?>
+
+<style type="text/css">
+body {background-color: #ffffff; color: #000000;}
+body, td, th, h1, h2 {font-family: sans-serif;}
+pre {margin: 0px; font-family: monospace;}
+table {border-collapse: collapse;}
+.center {text-align: center;}
+.center table { margin-left: auto; margin-right: auto; text-align: left;}
+.center th { text-align: center !important; }
+.p {text-align: left;}
+.e {background-color: #ccccff; font-weight: bold; color: #000000;}
+.h {background-color: #9999cc; font-weight: bold; color: #000000;}
+.v {background-color: #cccccc; color: #000000;}
+.vr {background-color: #cccccc; text-align: right; color: #000000;}
+img {float: right; border: 0px;}
+</style>
