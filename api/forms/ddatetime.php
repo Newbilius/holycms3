0ec496fc99d7adfab9bc7630ce23ba6e3f97f22a
Newbilius <newbilius@gmail.com>
2013-03-25 20:47:54 +0600

    fix В форму "дата-время" теперь можно передавать и строку, а не массив.

--- api/forms/ddatetime.php
+++ api/forms/ddatetime.php
@@ -34,22 +34,22 @@ class CForm_ddatetime extends CForm_Text {
         ?>
         <input id=<?= $name ?> name=<?= $name ?>[0] value="<?= $_POST[$name][0] ?>" style="float:left;">
         <select id=<?= $name ?> name=<?= $name ?>[1] style="float:left;">
-        <?
-        for ($i = 0; $i <= 23; $i++) {
-            ?>
-                <option value=<?= $i ?> <? if ($i == $_POST[$name][1]) { ?>selected<? }; ?>>
-                <?= DateTimePrefix($i) ?>
-                </option>
-                    <?
-                };
+            <?
+            for ($i = 0; $i <= 23; $i++) {
                 ?>
+                <option value=<?= $i ?> <? if ($i == $_POST[$name][1]) { ?>selected<? }; ?>>
+                    <?= DateTimePrefix($i) ?>
+                </option>
+                <?
+            };
+            ?>
         </select>
         <select id=<?= $name ?> name=<?= $name ?>[2] style="float:left;">
             <?
             for ($i = 0; $i <= 59; $i++) {
                 ?>
                 <option value=<?= $i ?> <? if ($i == $_POST[$name][2]) { ?>selected<? }; ?>>
-                <?= DateTimePrefix($i) ?>
+                    <?= DateTimePrefix($i) ?>
                 </option>
                 <?
             };
@@ -60,75 +60,77 @@ class CForm_ddatetime extends CForm_Text {
             for ($i = 0; $i <= 59; $i++) {
                 ?>
                 <option value=<?= $i ?> <? if ($i == $_POST[$name][3]) { ?>selected<? }; ?>>
-                <?= DateTimePrefix($i) ?>
+                    <?= DateTimePrefix($i) ?>
                 </option>
                 <?
             };
             ?>
         </select>
-            <?
-        }
+        <?
+    }
 
-        function Edit($name, $data, $add, $multiple = false) {
-            $this->HTML($name);
-            $base_date = explode(" ", $data[$name]);
-            $base_time = explode(":", $base_date[1]);
+    function Edit($name, $data, $add, $multiple = false) {
+        $this->HTML($name);
+        $base_date = explode(" ", $data[$name]);
+        $base_time = explode(":", $base_date[1]);
 
-            if ($base_date[0] == "0000-00-00")
-                $base_date[0] = date("Y-m-d");
-            ?>
+        if ($base_date[0] == "0000-00-00")
+            $base_date[0] = date("Y-m-d");
+        ?>
         <input id=<?= $name ?> name=<?= $name ?>[0] value="<?= $base_date[0] ?>" style="float:left;">
         <select id=<?= $name ?> name=<?= $name ?>[1] style="float:left;">
-        <?
-        for ($i = 0; $i <= 23; $i++) {
-            ?>
+            <?
+            for ($i = 0; $i <= 23; $i++) {
+                ?>
                 <option value=<?= $i ?> <? if ($i == $base_time[0]) { ?>selected<? }; ?>>
-            <?= DateTimePrefix($i) ?>
+                    <?= DateTimePrefix($i) ?>
                 </option>
                 <?
             };
             ?>
         </select>
         <select id=<?= $name ?> name=<?= $name ?>[2] style="float:left;">
-                <?
-                for ($i = 0; $i <= 59; $i++) {
-                    ?>
+            <?
+            for ($i = 0; $i <= 59; $i++) {
+                ?>
                 <option value=<?= $i ?> <? if ($i == $base_time[1]) { ?>selected<? }; ?>>
-                <?= DateTimePrefix($i) ?>
+                    <?= DateTimePrefix($i) ?>
                 </option>
                 <?
             };
             ?>
         </select>
         <select id=<?= $name ?> name=<?= $name ?>[3] style="float:left;">
-                <?
-                for ($i = 0; $i <= 59; $i++) {
-                    ?>
+            <?
+            for ($i = 0; $i <= 59; $i++) {
+                ?>
                 <option value=<?= $i ?> <? if ($i == $base_time[2]) { ?>selected<? }; ?>>
-                <?= DateTimePrefix($i) ?>
+                    <?= DateTimePrefix($i) ?>
                 </option>
-            <?
-        };
-        ?>
-        </select>
-            <?
-        }
-
-        function AfterEdit($name, $value, $add) {
-            return $value[0] . " " . $value[1] . ":" . $value[2] . ":" . $value[3];
-        }
-
-        function BeforeAdd($name, $value, $add) {
-            //echo $value[0]." ".$value[1].":".$value[2].":".$value[3];
-//		die();
-            return $value[0] . " " . $value[1] . ":" . $value[2] . ":" . $value[3];
-        }
-
-        function Filter($name) {
-            if (!isset($_GET['filter'][$name]))
-                $_GET['filter'][$name] = "";
-            $this->HTML($name);
+                <?
+            };
             ?>
+        </select>
+        <?
+    }
+
+    function AfterEdit($name, $value, $add) {
+        if (!is_array($value))
+            return $value;
+        return $value[0] . " " . $value[1] . ":" . $value[2] . ":" . $value[3];
+    }
+
+    function BeforeAdd($name, $value, $add) {
+        if (!is_array($value))
+            return $value;
+        return $value[0] . " " . $value[1] . ":" . $value[2] . ":" . $value[3];
+    }
+
+    function Filter($name) {
+        if (!isset($_GET['filter'][$name]))
+            $_GET['filter'][$name] = "";
+        $this->HTML($name);
+        ?>
         <input id=<?= $name ?> name=filter[<?= $name ?>] value="<?= $_GET['filter'][$name]; ?>" style="width:90%">
         <?
     }
